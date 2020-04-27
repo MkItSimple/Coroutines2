2572fb2204181e39cda8515ddd501fd18c3621a6
MkItSimple <choanania1@gmail.com>
2020-04-27 09:19:07 +0800

    BeginnerExample-2

--- app/src/main/java/com/example/coroutines2/MainActivity.kt
+++ app/src/main/java/com/example/coroutines2/MainActivity.kt
@@ -6,13 +6,16 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
     private  val RESULT_1 = "Result #1"
+    private  val RESULT_2 = "Result #2"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +30,37 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setNewText(input: String){
+        val newText = text.text.toString() + "\n$input"
+        text.text = newText
+    }
+
+    private suspend fun setTextOnMainThread(input: String){
+        withContext(Main){
+            setNewText(input)
+        }
+    }
+
     private suspend fun fakeApiRequest(){
         val result1 = getResult1FromApi()
         println("debug: $result1")
+        setTextOnMainThread(result1)
+
+        val result2 = getResult2FromApi()
+        println("debug: $result2")
+        setTextOnMainThread(result2)
     }
 
     private suspend fun getResult1FromApi(): String{
         logThread("getResult1FromApi")
         delay(1000)
         return RESULT_1
+    }
+
+    private suspend fun getResult2FromApi(): String{
+        logThread("getResult2FromApi")
+        delay(1000)
+        return RESULT_2
     }
 
     private fun logThread(methodName: String) {
